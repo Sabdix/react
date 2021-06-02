c27aa59a1937fcf8e8dd3f4cbf2f4feb940fecd3
Sabdi Abraham Pantoja Orozco <32939564+Sabdix@users.noreply.github.com>
2021-06-02 16:48:01 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './styles.css';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+
+const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2021, 8, 13)
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 3, 11)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 12)
+    }
+  ];
+
+  return (
+    <div>
+      <NewExpense />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
