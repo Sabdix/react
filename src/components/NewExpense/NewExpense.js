cf6971a6644532166987d0553503e3a65d599aff
Sabdi Abraham Pantoja Orozco <32939564+Sabdix@users.noreply.github.com>
2021-06-22 21:38:15 -0500

    Functionality to the form and emitting and event

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './NewExpense.css';
-import ExpenseForm from './ExpenseForm';
+import React from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
 const NewExpense = () => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    console.log(expenseData);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
